4ab5578668380a598c0feb4b07bcebf8eb9ed755
Matt <TheDraca@users.noreply.github.com>
2021-08-22 21:08:49 +0100

    Create + Update
    Forgot to commit this last night loll, contains functions to read/write settings for OTS as well as generate the config file if needed

---
+++ JsonControl.py
@@ -0,0 +1,29 @@
+import json
+
+def GenJSONFile(Filename="OTS.json"):
+    with open(Filename, "w") as JSONFile:
+        FileContents={
+            "API": {
+                "Username" : "",
+                "APIKey" : ""
+            },
+            "Settings": {
+                "DefaultTimeOut": "3d"
+            }
+            }
+        json.dump(FileContents,JSONFile)
+
+def GetCurrentContents(Filename="OTS.json"):
+    with open(Filename, "r") as JSONFile:
+        return json.load(JSONFile)
+
+def GetItem(ItemKey,ItemName,Filename="OTS.json"):
+    with open(Filename, "r") as JSONFile:
+        return (json.load(JSONFile)[ItemKey][ItemName])
+
+def SaveItem(ItemKey, ItemName, Item, Filename="OTS.json"):
+    Contents=GetCurrentContents(Filename)
+    with open(Filename, "w+") as JSONFile:
+        (Contents[ItemKey])[ItemName] = Item
+        json.dump(Contents,JSONFile)
+        
