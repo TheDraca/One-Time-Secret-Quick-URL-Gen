1418b54f5089f979f56ffee68562f9249b55fddb
Matt <TheDraca@users.noreply.github.com>
2021-08-24 20:41:01 +0100

    Don't copy json + Some bad wording

--- unixInstall.sh
+++ unixInstall.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+#Create directory and copy needed files
 mkdir -p $HOME/.ots
 cp ots.pyw $HOME/.ots/
 cp JsonControl.py $HOME/.ots/
-cp OTS.json $HOME/.ots/
+
+#Test if we already have a  json file
+if test -f "$HOME/.ots/OTS.json"; then
+    echo "WARNING: You already have OTS.json file watch out if there have been any changes to the file"
+fi
 
 #Check if theres a ZSH rc file
 if test -f "$HOME/.zshrc"; then
@@ -10,12 +16,13 @@ if test -f "$HOME/.zshrc"; then
         echo "ZSH RC File found but alias not setup, creating ots alias now"
         echo "alias ots='(cd $HOME/.ots && python3 ots.pyw && cd - > /dev/null)'" >> $HOME/.zshrc
     fi
+#Check if we don't have a bash profile OR a zsh profile and warn about it
 else ! test -f "$HOME/.bash_profile"
-    echo "No ZSH File or bash profile, guessing you are using bash and putting the alisa there"
+    echo "No ZSH File or bash profile, guessing you are using bash and putting the alias in a new bash_profile"
     touch "$HOME/.bash_profile"
 fi
 
-#Check if there's a bash profile file add alias to it
+#If there's a bash profile file add alias to it
 if test -f "$HOME/.bash_profile"; then
     if ! grep -q "alias ots='(cd $HOME/.ots && python3 ots.pyw && cd - > /dev/null)'" $HOME/.bash_profile ; then
         echo "Bash RC File found but alias not setup, creating ots alias now"
@@ -25,5 +32,5 @@ fi
 
 
 echo "OTS should now been installed to $HOME/.ots"
-echo "You may now delete these files, you may need to restart your terminal before the ots commanded works. If it doesn't add this alias to whatever shell you use:
+echo "You may now delete these files, you may need to restart your terminal before the ots command works, if it doesn't add this alias to whatever shell you use:
 alias ots='(cd $HOME/.ots && python3 ots.pyw && cd - > /dev/null)' "
