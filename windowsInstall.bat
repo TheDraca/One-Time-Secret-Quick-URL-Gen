6371537d6a669aeb2f51c70ddc6f694bf34fbf2e
Matt <TheDraca@users.noreply.github.com>
2022-07-30 15:41:31 +0100

    Update windowsInstall.bat

--- windowsInstall.bat
+++ windowsInstall.bat
@@ -18,16 +18,17 @@ copy ots.pyw "%localappdata%\ots\"
 copy JsonControl.py "%localappdata%\ots\" 
 copy ots.lnk "%AppData%\Microsoft\Windows\Start Menu\Programs"
 
-if exist "%localappdata%\ots\OTS.json" echo "WARNING: You already have OTS.json file watch out if there have been any changes to the file "
-
+if exist "%localappdata%\ots\OTS.json" echo "WARNING: You already have OTS.json file watch out if there have been any changes to the file"
 
 
 echo "OTS should now been installed to %localappdata%\ots\"
 echo "You may now delete these files, run OTS from your start menu now to setup your API Username/Password if needed"
 pause
-goto: eof
+goto eof
 
 
 :NoPython3
 echo "Python3 is not installed, please first download it here: https://www.python.org/downloads/"
 pause
+
+:eof
